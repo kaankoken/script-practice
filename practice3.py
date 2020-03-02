e949f2a48b07aa879f69c634f412e10e8389652d
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2020-03-02 14:39:00 +0200

    5th question is completed

--- practice3.py
+++ practice3.py
@@ -1,10 +1,12 @@
 import re
 
+#1
 def capt_first_letter():
     #sentence = input()
     #split = sentence.split(" ")
     print([i[0].upper() + i[1:] for i in input().split(" ")])
 
+#2
 def count_char(dic, key):
     if key in dic:
         dic[key] = dic[key] + 1
@@ -12,12 +14,17 @@ def count_char(dic, key):
         pass
     else:
         dic[key] = 1
-
+#3
 def occurence():
     sentence = input()
     factor = int(input())
     mod = len(sentence) / factor
-    
+#5
+def yearReg():
+    year = input("Enter the year: ")
+    x = re.split("(\d{2})[.|-](\d{2})[.|-](\d{4}|\d{2})", year)
+    print(x)
+
 def subs():
     #substitution
     #"abc def ghi" -> "word word word"
@@ -57,7 +64,7 @@ def main():
     #for key in sentence:
     #    count_char(dic, key)
     #test()
-
+    yearReg()
 
 if __name__ == "__main__":
     main()
